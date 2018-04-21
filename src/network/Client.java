e1e32e8e3909e59007cb38a841b25e487b2e218e
Makodan <danymako@gmail.com>
2018-04-21 02:03:54 +0200

    Server,Client class completion

--- src/network/Client.java
+++ src/network/Client.java
@@ -1,32 +1,39 @@
 package network;
 
+import model.FallObject;
+import model.Player;
+
 import java.io.IOException;
-import java.io.ObjectInputStream;
+//import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.List;
+
 
 public class Client {
+    private String ip;
     private Socket socket = null;
-    private ObjectInputStream inputStream = null;
+    //private ObjectInputStream inputStream = null;
     private ObjectOutputStream outputStream = null;
     private boolean isConnected = false;
+    private testobject to;
 
-    public Client() {
+    public Client(String ipaddress) {
+        this.ip = ipaddress;
 
     }
 
-    public void communicate() {
+
+    public void ConnectToServer() {
 
         while (!isConnected) {
             try {
-                socket = new Socket("localHost", 7777);
-                System.out.println("Connected");
+                System.out.println("Try to connect to:" + ip);
+                socket = new Socket(ip, 7777);
+                System.out.println("Connected!");
                 isConnected = true;
                 outputStream = new ObjectOutputStream(socket.getOutputStream());
-                testobject to = new testobject(1, "Bijoy");
-                System.out.println("Object to be written = " + to);
-                outputStream.writeObject(to);
 
             } catch (SocketException se) {
                 se.printStackTrace();
@@ -34,6 +41,54 @@ public class Client {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    public void Disconnect() {
+
+        try {
+            System.out.println("Try to close socket, HOST:" + socket.getInetAddress());
+            outputStream.close();
+            socket.close();
+            System.out.println("Success!");
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void SendPlayerData(Player player) {
+
+        System.out.println("Player to be written = " + player);
+        try {
+            outputStream.writeObject(player);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void SendFallObjectList(List<FallObject> list) {
+        System.out.println("List to be written = " + list);
+
+        try {
+            outputStream.writeObject(list);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void SendAll(Player player, List<FallObject> list){
+
+        System.out.println("Player and list sending = " + player + list);
+        try {
+            outputStream.writeObject(player);
+            outputStream.writeObject(list);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/network/Server.java
+++ src/network/Server.java
@@ -1,19 +1,31 @@
 package network;
 
+import model.FallObject;
+import model.Player;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.List;
 
 public class Server implements Runnable {
     private ServerSocket serverSocket = null;
     private Socket socket = null;
     private ObjectInputStream inStream = null;
-
+    private Player player;
+    private List<FallObject> fallobjectlist;
     public Server() {
 
     }
 
+    public Player getPlayer() {
+        return player;
+    }
+
+    public List<FallObject> getFallobjectlist() {
+        return fallobjectlist;
+    }
 
     @Override
     public void run() {
@@ -22,17 +34,17 @@ public class Server implements Runnable {
             socket = serverSocket.accept();
             System.out.println("Connected");
             inStream = new ObjectInputStream(socket.getInputStream());
-            testobject to;
+
             while (true) {
 
-                to = (testobject) inStream.readObject();
-                System.out.println("Object received = " + to);
-                System.out.println(to.id);
-                System.out.println(to.value);
+                player = (Player) inStream.readObject();
+                fallobjectlist = (List<FallObject>) inStream.readObject();
+                //System.out.println("Object received = " + player);
+                //System.out.println("Object received = " + fallobjectlist);
+                System.out.println(fallobjectlist.size());
+                System.out.println(player.getAlcoholLevel());
+
             }
-
-
-
 
         } catch (SocketException se) {
             System.exit(0);
